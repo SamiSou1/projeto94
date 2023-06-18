797f07ab6c95b10d0f615241e362e547fed93b9f
SamiSou1 <121313494+SamiSou1@users.noreply.github.com>
2023-06-18 14:02:27 -0300

    Add files via upload

---
+++ kwitterRoom.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWvbrip1kl0Df4Susay2nbqSeKbWzt3xc",
+  authDomain: "projeto94-c9349.firebaseapp.com",
+  projectId: "projeto94-c9349",
+  storageBucket: "projeto94-c9349.appspot.com",
+  messagingSenderId: "944337034442",
+  appId: "1:944337034442:web:cfac916379739a6b98a833"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
